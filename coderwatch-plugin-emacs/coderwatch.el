8806502d1b49a82b7eeeb922cdb2602c85166efe
Clément Sibille <clement.sibille@protonmail.com>
2020-09-27 09:54:40 +0200

    Add a basic Emacs plugin that finds a .coderwatch file in the tree

---
+++ coderwatch-plugin-emacs/coderwatch.el
@@ -0,0 +1,41 @@
+;;; coderwatch.el --- Allow Coderwatch support for emacs
+;; Author: Lisible
+;; Created: 27 Nov 2020
+;; License: MIT
+
+;;; Commentary:
+;; This Emacs plugin allows you to use Emacs with Coderwatch.
+
+;;; Code:
+
+(require 'files)
+(locate-dominating-file (buffer-file-name) ".coderwatch")
+
+(define-minor-mode coderwatch-mode
+  "Global minor mode for coderwatch"
+  nil nil nil
+  :require 'coderwatch
+  :global t
+  :group 'coderwatch
+  :after-hook
+  (progn
+    (cond
+     (coderwatch-mode
+      (coderwatch-enable))
+     (t
+      (coderwatch-disable)))))
+
+(defconst coderwatch-project-file-name ".coderwatch")
+
+(defun coderwatch-enable ()
+  (message (coderwatch-get-project-file-path)))
+
+(defun coderwatch-disable () ())
+
+(defun coderwatch-get-project-file-path ()
+  (concat
+   (locate-dominating-file (buffer-file-name) coderwatch-project-file-name)
+   coderwatch-project-file-name))
+
+(provide 'coderwatch)
+;;; coderwatch.el ends here
